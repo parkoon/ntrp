cec15f75d44471aa480c74b62250f3532c8b10a6
parkoon <devparkoon@gmail.com>
2021-02-25 21:59:50 +0900

    feat: styled component with styled-reset

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 
+import {createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  /* other styles */
+`
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <GlobalStyle />
+    <Component {...pageProps} />
+  </>
 }
 
 export default App
