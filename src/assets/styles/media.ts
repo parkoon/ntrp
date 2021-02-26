75e4860377817f10692317f004ef32c7f434ef51
parkoon <devparkoon@gmail.com>
2021-02-26 16:15:52 +0900

    feat: add media query for styled component

---
+++ src/assets/styles/media.ts
@@ -0,0 +1,21 @@
+import { css } from 'styled-components'
+
+// Ref. https://medium.com/@samuelresua/easy-media-queries-in-styled-components-690b78f50053
+const sizes: { [key: string]: number } = {
+  huge: 1200,
+  large: 992,
+  medium: 768,
+  small: 576,
+}
+
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (literals: TemplateStringsArray, ...placeholders: any[]) =>
+    css`
+      @media (max-width: ${sizes[label]}px) {
+        ${css(literals, ...placeholders)};
+      }
+    `.join('')
+  return acc
+}, {} as Record<'huge' | 'large' | 'medium' | 'small', (l: TemplateStringsArray, ...p: any) => string>)
+
+export default media
